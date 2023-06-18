da8b6b7549445182d388119c62ee1e515d68b5c2
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2023-06-18 16:41:27 +0200

    add ex_prime + solution

---
+++ ex_prime.py
@@ -0,0 +1,15 @@
+"""
+Your task: Check if a given number is a prime number
+
+"""
+
+def check_prime(number):
+    """
+    Return True, if prime number, otherwise return False
+    """
+    pass
+
+if __name__ == '__main__':
+    print(check_prime(12))  # not prime number
+    print(check_prime(15))  # not prime number
+    print(check_prime(3))  #  prime number
